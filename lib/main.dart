db75395134346263a8e2522a483c026262441d42
Mahmoud-Niazy <mahmoudniazy33@gmail.com>
2023-02-27 19:51:01 +0200

    'Auto play'

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:xo/cubit.dart';
-import 'package:xo/2_players_screen.dart';
-
-import '1_player_screen.dart';
 import 'home_screen.dart';
 
 void main(){
